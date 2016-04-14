1ef87b91a04e0351e550b36c38c5c3f5ccbeddb2
Michael Aubrey <mcaubrey511@gmail.com>
2016-04-14 02:19:16 +0000

    commented code, added blank reset method

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 /* global $ */
 
+// Clock object is self contained, and has everything
+// necessary to make all the calculations for the project.
+// All the GUI stuff is handled in other functions/the
+// main document.ready function.
 var clock = {
-  isStopped: true,
-  isResting: false,
-  internalTimer: setInterval(function() {
+  resetRequested: false, // used to evaluate whether the reset button was pressed.
+  isStopped: true, // if isStopped is false, time will be acounted as "timeSpentWorking"
+  isResting: false, // if isResting is true AND isStopped is false, time is counte das "timeNotWorking"
+  // Internal timer for clock calculations, runs independant of the timer used to update the interface.
+  internalTimer: setInterval(function() { 
     if (!clock.isStopped && !clock.isResting) {
       clock.timeSpentWorking = clock.timeSpentWorking + 500;
     } else if (!clock.isStopped && clock.isResting) {
@@ -59,12 +65,15 @@ var clock = {
     clock.taskBreakLength = clock.taskBreakLength + breakTime;
   },
   reset: function() {
-
+    // Incomplete (obviously) this function should
+    // Reset the app to its initial state.
   }
 }
 
 function msToTime(duration) {
-
+// http://stackoverflow.com/questions/9763441/milliseconds-to-time-in-javascript
+// Took this function from Stack Overflow. Consider updating it later to display
+// "0 hours, 1 minute, and 25 seconds" or something along those lines.
   var milliseconds = parseInt((duration % 1000) / 100),
     seconds = parseInt((duration / 1000) % 60),
     minutes = parseInt((duration / (1000 * 60)) % 60),
@@ -78,6 +87,8 @@ function msToTime(duration) {
 }
 
 function updateInterface() {
+  // This function puts all the information from the clock object onto the page
+  // for the user to read.
   $(".current-time").html(new Date());
   if (clock.estimatedCompletionTime !== 0) {
     $(".estimated-completion-time").html(new Date(clock.estimatedCompletionTime));
@@ -88,16 +99,24 @@ function updateInterface() {
   if (!$(".tasks-count").is(':focus') && $(".tasks-count").val() > 0) {
     $(".tasks-count").val(clock.tasksLeft);
   }
+  if (clock.resetRequested) {
+    // perform addition UI updates to reset things
+    // to their initial state.
+  }
 }
 
 $(document).ready(function() {
+  // Hide everything but the start button.
   $(".finish").hide();
   $(".break").hide();
   $(".resume").hide();
   $(".reset").hide();
-
+  
+  // Run updateInterface() as soon as the page loads
+  // (later it will be run in mainTimer)
   updateInterface();
 
+  // If number of tasks are specified, enable buttons.
   $(".tasks-count").on("input", function() {
     if (parseInt($(".tasks-count").val()) > 0) {
       $("button").prop("disabled", false);
@@ -107,9 +126,9 @@ $(document).ready(function() {
       $("button").prop("disabled", true);
       $("p.tip").fadeIn("slow");
     }
-
   });
 
+  // mainTimer updates the interface every half second.
   var mainTimer = setInterval(function() {
     updateInterface();
   }, 500);
@@ -119,6 +138,9 @@ $(document).ready(function() {
     $(".finish").show();
     $(".break").show();
     $(".start").hide();
+    // When reset() method is added to clock, uncomment the following
+    // to show the reset button.
+    // $(".reset").show();
   });
   $(".resume").click(function() {
     clock.resumeTask();
@@ -140,6 +162,9 @@ $(document).ready(function() {
   });
 
   function parseTaskLengths(arr) {
+    // parse task length data to make it more human readable,
+    // and also to give it spaces so that the array wraps
+    // when displayed in the dev area.
     let data = [];
     arr.forEach(function(i) {
       data.unshift(msToTime(i));
@@ -148,6 +173,11 @@ $(document).ready(function() {
   }
 
   var devTimer = setInterval(function() {
+    // I wanted to see the variables updating live while working on
+    // this project. However, the information is interseting, so
+    // I'm thinking of just leaving the dev area visible at least
+    // in this app's current form. If I eventually update to a
+    // different UI, I'll probably present the information differently.
     var spacer = " &nbsp;&nbsp;";
     $(".dev").html("taskStartTime: " + clock.taskStartTime +
       spacer + "taskEndTime: " + clock.taskEndTime +
